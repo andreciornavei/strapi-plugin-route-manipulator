a39b6e0a00a4554089bf2acc05490d44946d2896
account1 <andre.ciornavei@gmail.com>
2021-09-13 13:12:06 -0300

    implemented feature to aloc manipulator before or after some policy

--- middlewares/route-manipulator/index.js
+++ middlewares/route-manipulator/index.js
@@ -1,19 +1,42 @@
 const _ = require('lodash');
 
+function insertAt(array, index, ...elementsArray) {
+  array.splice(index, 0, ...elementsArray);
+}
+
+function appendPolicy(value) {
+  // if config contains arranges, then check if it is an object
+  // and handle beforePolicy or afterPolicy to attach the handler
+  // on the desired policy position accoring business rule
+  if (_.get(value, "config.manipulator.arrange")) {
+    for (const arrange of Object.values(value.config.manipulator.arrange)) {
+      for (const arrangeEntry of Object.values(arrange)) {
+        if (_.get(arrangeEntry, "beforePolicy")) {
+          const policyIndex = value.config.policies.findIndex(entry => entry == _.get(arrangeEntry, "beforePolicy"))
+          insertAt(value.config.policies, policyIndex, 'plugins::route-manipulator.handle')
+        }
+        if (_.get(arrangeEntry, "afterPolicy")) {
+          const policyIndex = value.config.policies.findIndex(entry => entry == _.get(arrangeEntry, "afterPolicy"))
+          insertAt(value.config.policies, policyIndex + 1, 'plugins::route-manipulator.handle')
+        }
+      }
+    }
+  }
+  value.config.policies.unshift('plugins::route-manipulator.handle');
+}
+
 module.exports = strapi => {
   return {
     initialize() {
       // Attach route-manipulator handler to api actions
       _.forEach(_.get(strapi, "config.routes", []), value => {
-        if (_.get(value.config, 'manipulator')) {
-          value.config.policies.unshift('plugins::route-manipulator.handle');
-        }
+        if (_.get(value.config, 'manipulator')) appendPolicy(value)
       });
       // Attach route-manipulator handler to extensions actions
       if (strapi.extensions) {
         _.forEach(strapi.extension, plugin => {
           _.forEach(_.get(plugin, "config.routes", []), value => {
-            if (_.get(value.config, 'manipulator')) value.config.policies.unshift('plugins::route-manipulator.handle');
+            if (_.get(value.config, 'manipulator')) appendPolicy(value)
           });
         });
       }
