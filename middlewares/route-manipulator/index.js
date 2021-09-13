a1e00d65180e725db857269c4b27d7070db7317b
account1 <andre.ciornavei@gmail.com>
2021-09-13 12:04:10 -0300

    arrange policy to the first route policy, allowing other policies to access ha manipulator result

--- middlewares/route-manipulator/index.js
+++ middlewares/route-manipulator/index.js
@@ -3,15 +3,17 @@ const _ = require('lodash');
 module.exports = strapi => {
   return {
     initialize() {
-      // Attach validations/sanitizors to api actions
+      // Attach route-manipulator handler to api actions
       _.forEach(_.get(strapi, "config.routes", []), value => {
-        if (_.get(value.config, 'manipulator')) value.config.policies.push('plugins::route-manipulator.handle');
+        if (_.get(value.config, 'manipulator')) {
+          value.config.policies.unshift('plugins::route-manipulator.handle');
+        }
       });
-      // Attach validations/sanitizors to extensions actions
+      // Attach route-manipulator handler to extensions actions
       if (strapi.extensions) {
         _.forEach(strapi.extension, plugin => {
           _.forEach(_.get(plugin, "config.routes", []), value => {
-            if (_.get(value.config, 'manipulator')) value.config.policies.push('plugins::route-manipulator.handle');
+            if (_.get(value.config, 'manipulator')) value.config.policies.unshift('plugins::route-manipulator.handle');
           });
         });
       }
